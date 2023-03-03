4b6d3149bd2be7076820abe4c9b266d634bafbfe
Anna S <selivanovaanya@gmail.com>
2023-03-04 00:21:21 +0300

    corrected

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import requests
 url = "https://akabab.github.io/superhero-api/api/all.json"
 resp = requests.get(url)
 json_data = resp.json()
-dictionary ={}
+dictionary = {}
 for hero in json_data:
     if hero["name"] == "Hulk" or hero["name"] == "Captain America" or hero["name"] == "Thanos":
         a = hero["name"]
         b = hero["powerstats"]["intelligence"]
         dictionary[a] = b
-print (dictionary)
+print(dictionary)
 Hulk = int(dictionary['Hulk'])
 Captain_America = int(dictionary['Captain America'])
 Thanos = int(dictionary['Thanos'])
@@ -19,4 +19,5 @@ if Captain_America > mx:
 if Thanos > mx:
     mx = Thanos
 name = (next(i for i, value in dictionary.items() if value == mx))
-print(f"From Hulk with intelligence = {Hulk}, Captain America with intelligence = {Captain_America}, Thanos with intelligence = {Thanos} most intelligent is {name}.")
+print(f"From Hulk with intelligence = {Hulk}, Captain America with intelligence = {Captain_America},"
+      f"Thanos with intelligence = {Thanos} - most intelligent is {name}.")
